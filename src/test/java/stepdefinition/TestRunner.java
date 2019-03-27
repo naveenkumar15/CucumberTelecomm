5b0b6da3bba27210db27ffef759644c7acb84a60
naveenkumar15 <navinag63@gmail.com>
2019-03-27 19:05:46 +0530

    First Commit

--- src/test/java/stepdefinition/TestRunner.java
+++ src/test/java/stepdefinition/TestRunner.java
@@ -1,12 +1,11 @@
 package stepdefinition;
-
+import cucumber.api.junit.*;
 import org.junit.runner.RunWith;
-import cucumber.api.junit.Cucumber;
 import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = { "stepdefinition" }, plugin = { "html:target",
-		"json:src/test/java/pack2/report.json" }, tags = {}, monochrome = true, dryRun = false, strict = true)
+@CucumberOptions(features="src/test/resources/features", glue = { "stepdefinition" }, plugin = { "html:target",
+		"json:src/test/java/pack2/report.json"}, tags = {}, monochrome = true, dryRun = false, strict = true)
 
 public class TestRunner {
 
